ced85c402d75b8aa7f3a0318a3b512240549b90c
Alireza Moradi <alirezamoradi2ns@gmail.com>
2024-08-01 05:11:57 +0330

    add 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  content: ["./index.html", "./src/**/*.{js,jsx}",
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+  ],
   mode: "jit",
   theme: {
     extend: {
